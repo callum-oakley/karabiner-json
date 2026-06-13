f06137173789ad3e6f9e6e56e24645a33043f239
Callum Oakley <hello@callumoakley.net>
2026-06-13 16:34:39 +0100

    all symbols on base or num

--- karabiner.js
+++ karabiner.js
@@ -42,16 +42,16 @@ function map(from, to, opts) {
     map("w", "f2", { layer: "fn" }),
     map("e", "f3", { layer: "fn" }),
     map("r", "f4", { layer: "fn" }),
+    map("t", "f5", { layer: "fn" }),
 
-    map("a", "f5", { layer: "fn" }),
-    map("s", "f6", { layer: "fn" }),
-    map("d", "f7", { layer: "fn" }),
-    map("f", "f8", { layer: "fn" }),
+    map("a", "f6", { layer: "fn" }),
+    map("s", "f7", { layer: "fn" }),
+    map("d", "f8", { layer: "fn" }),
+    map("f", "f9", { layer: "fn" }),
+    map("g", "f10", { layer: "fn" }),
 
-    map("z", "f9", { layer: "fn" }),
-    map("x", "f10", { layer: "fn" }),
-    map("c", "f11", { layer: "fn" }),
-    map("v", "f12", { layer: "fn" }),
+    map("z", "f11", { layer: "fn" }),
+    map("x", "f12", { layer: "fn" }),
 
     // right hand
     map("j", "left_shift", { layer: "fn" }),
@@ -59,20 +59,24 @@ function map(from, to, opts) {
 
     // num -----------------------------------------------------------------------------------------
     // left hand
+    map("tab", "grave_accent_and_tilde", { layer: "num" }),
     map("q", "1", { layer: "num" }),
     map("w", "2", { layer: "num" }),
     map("e", "3", { layer: "num" }),
     map("r", "4", { layer: "num" }),
+    map("t", "5", { layer: "num" }),
 
-    map("a", "5", { layer: "num" }),
-    map("s", "6", { layer: "num" }),
-    map("d", "7", { layer: "num" }),
-    map("f", "8", { layer: "num" }),
+    map("a", "6", { layer: "num" }),
+    map("s", "7", { layer: "num" }),
+    map("d", "8", { layer: "num" }),
+    map("f", "9", { layer: "num" }),
+    map("g", "0", { layer: "num" }),
 
-    map("z", "9", { layer: "num" }),
-    map("x", "0", { layer: "num" }),
-    map("c", "hyphen", { layer: "num" }),
-    map("v", "equal_sign", { layer: "num" }),
+    map("z", "hyphen", { layer: "num" }),
+    map("x", "equal_sign", { layer: "num" }),
+    map("c", "backslash", { layer: "num" }),
+    map("v", "open_bracket", { layer: "num" }),
+    map("b", "close_bracket", { layer: "num" }),
 
     // right hand
     map("u", "volume_decrement", { layer: "num" }),
@@ -85,14 +89,12 @@ function map(from, to, opts) {
     map("m", "display_brightness_decrement", { layer: "num" }),
     map("comma", "display_brightness_increment", { layer: "num" }),
 
-    map("spacebar", ["control", "spacebar"], { layer: "num" }),
-
     // nav -----------------------------------------------------------------------------------------
     // left hand
     map("e", ["shift", "command", "open_bracket"], { layer: "nav" }),
     map("r", ["shift", "command", "close_bracket"], { layer: "nav" }),
 
-    map("s", "return_or_enter", { layer: "nav" }),
+    map("s", "escape", { layer: "nav" }),
     map("d", "left_option", { layer: "nav" }),
     map("f", "left_shift", { layer: "nav" }),
 
@@ -108,18 +110,12 @@ function map(from, to, opts) {
     map("j", "left_arrow", { layer: "nav" }),
     map("k", "down_arrow", { layer: "nav" }),
     map("l", "right_arrow", { layer: "nav" }),
-    map("semicolon", "grave_accent_and_tilde", { layer: "nav" }),
-
-    map("m", "open_bracket", { layer: "nav" }),
-    map("comma", "close_bracket", { layer: "nav" }),
-    map("period", "semicolon", { layer: "nav" }),
-    map("slash", "backslash", { layer: "nav" }),
+    map("semicolon", "return_or_enter", { layer: "nav" }),
 
     // base ----------------------------------------------------------------------------------------
     map("caps_lock", { layer: "nav" }),
     map("left_control", { layer: "nav" }), // For HHKB
-    map("open_bracket", "escape"),
-    map("semicolon", "quote"),
+    map("open_bracket", "quote"),
     map("quote", { layer: "num" }),
     map("right_command", "right_control"),
     map("grave_accent_and_tilde", "left_shift"), // For UK Macbook
